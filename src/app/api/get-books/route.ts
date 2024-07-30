28d9c07f98671ebec0b66918e92c68dea78201a1
Sumit <vermasumit923@gmail.com>
2024-07-30 23:57:49 +0530

    fix: removed caching of get-books api call

--- src/app/api/get-books/route.ts
+++ src/app/api/get-books/route.ts
@@ -6,19 +6,33 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     try {
         await dbConnect();
         const books = await BookModel.find();
-        return NextResponse.json({
+
+
+        const response = NextResponse.json({
             success: true,
             message: "Successfully fetched all the books",
             response: books
         }, {
             status: 200
         })
+
+        response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        response.headers.set('Pragma', 'no-cache');
+        response.headers.set('Expires', '0');
+        response.headers.set('Surrogate-Control', 'no-store');
+        return response;
     } catch(error: any) {
-        return NextResponse.json({
+        const response = NextResponse.json({
             success: false,
             message: "Error in getting the books"
         }, {
             status: 500
         })
+
+        response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        response.headers.set('Pragma', 'no-cache');
+        response.headers.set('Expires', '0');
+        response.headers.set('Surrogate-Control', 'no-store');
+        return response;
     }
 }
